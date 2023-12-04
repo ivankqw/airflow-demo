f21672661133599539ddc70225f4dee4e501a4c3
Koh Quan Wei Ivan <ivankohquanwei@gmail.com>
2023-12-04 23:23:42 +0800

    add example for taskflow api

---
+++ airflow-standalone/dags/tut8_dag_demo_taskflow.py
@@ -0,0 +1,63 @@
+from airflow.decorators import dag, task
+from datetime import datetime, timedelta
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2023, 12, 4),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=10)
+}
+
+@dag(dag_id='ecommerce_etl_taskflow', default_args=default_args, schedule_interval='@daily', dagrun_timeout=timedelta(minutes=60), catchup=False)
+def tutorial_taskflow_api_etl():
+    @task
+    def create_table():
+        create_table_sql = """
+        CREATE TABLE IF NOT EXISTS orders (
+            order_id INTEGER PRIMARY KEY,
+            customer_id INTEGER NOT NULL,
+            amount FLOAT NOT NULL,
+            status VARCHAR(50) NOT NULL
+        );
+        """
+        # Use the Airflow PostgresHook to execute the SQL
+        from airflow.providers.postgres.hooks.postgres import PostgresHook
+        hook = PostgresHook(postgres_conn_id='postgres_t8_nopwd')
+        hook.run(create_table_sql, autocommit=True)
+
+    @task
+    def extract():
+        # by default, any operator that return a value will push it to xcom
+        return {
+            'order_id': 4,
+            'customer_id': 123,
+            'amount': 100.0,
+            'status': 'completed'
+        }
+
+    @task
+    def transform(order_data_dict: dict):
+        order_data_dict['amount'] = order_data_dict['amount'] * 1.1  # Apply a tax
+        return order_data_dict
+
+    @task
+    def load(order_data_dict: dict):
+        load_sql = """
+        INSERT INTO orders (order_id, customer_id, amount, status)
+        VALUES (%(order_id)s, %(customer_id)s, %(amount)s, %(status)s);
+        """
+        # Use the Airflow PostgresHook to execute the SQL
+        hook = PostgresHook(postgres_conn_id='postgres_t8_nopwd')
+        hook.run(load_sql, parameters=order_data_dict, autocommit=True)
+
+    # task dependencies are defined in a straightforward way
+    create_table()
+    order_data = extract()
+    transformed_data = transform(order_data)
+    load(transformed_data)
+
+tutorial_etl_dag = tutorial_taskflow_api_etl()
