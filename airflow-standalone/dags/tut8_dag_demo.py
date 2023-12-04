cdfb1096080cd1c9886b347808faae12d7451c59
Koh Quan Wei Ivan <ivankohquanwei@gmail.com>
2023-12-04 22:34:16 +0800

    conn to pg

---
+++ airflow-standalone/dags/tut8_dag_demo.py
@@ -0,0 +1,78 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2023, 12, 4),
+    "email": ["airflow@example.com"],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=10)
+}
+
+mock_order_data = {
+    'order_id': 0,
+    'customer_id': 123,
+    'amount': 100.0,
+    'status': 'completed'
+}
+
+
+def extract_order_data():
+    # by default, any operator that return a value will push it to xcom
+    return mock_order_data
+
+
+def transform_order_data(ti):
+    order_data_dict = ti.xcom_pull(task_ids='extract')
+    order_data_dict['amount'] = order_data_dict['amount'] * 1.1  # Apply a tax
+    # pushes the transformed data to xcom
+    return order_data_dict
+
+
+with DAG('ecommerce_etl', default_args=default_args, schedule_interval='@daily', dagrun_timeout=timedelta(minutes=60)) as dag:
+
+    t0 = SQLExecuteQueryOperator(
+        task_id='create_table',
+        conn_id='postgres_t8_nopwd',
+        sql="""
+        CREATE TABLE IF NOT EXISTS orders (
+            order_id INTEGER PRIMARY KEY,
+            customer_id INTEGER NOT NULL,
+            amount FLOAT NOT NULL,
+            status VARCHAR(50) NOT NULL
+        );
+        """,
+        autocommit=True,
+    )
+
+    t1 = PythonOperator(
+        task_id='extract',
+        python_callable=extract_order_data,
+    )
+
+    t2 = PythonOperator(
+        task_id='transform',
+        python_callable=transform_order_data,
+    )
+
+    t3 = SQLExecuteQueryOperator(
+        task_id='load_order_data',
+        conn_id='postgres_t8_nopwd',
+        sql="""
+        INSERT INTO orders (order_id, customer_id, amount, status)
+        VALUES (%(order_id)s, %(customer_id)s, %(amount)s, %(status)s);
+        """,
+        autocommit=True,
+        parameters={
+            "order_id": "{{ ti.xcom_pull(task_ids='transform')['order_id'] }}",
+            "customer_id": "{{ ti.xcom_pull(task_ids='transform')['customer_id'] }}",
+            "amount": "{{ ti.xcom_pull(task_ids='transform')['amount'] }}",
+            "status": "{{ ti.xcom_pull(task_ids='transform')['status'] }}",
+        },
+    )
+
+    t0 >> t1 >> t2 >> t3
--- airflow-standalone/readme.md
+++ airflow-standalone/readme.md
@@ -4,5 +4,59 @@
 - Python 3.8 or higher
 - pip 
 
+**Note: It is recommended to use a virtual environment to install Airflow**
+
+Using venv (Pre-installed with Python)
+```
+cd ~/path/to/your/project
+python3 -m venv airflow-env
+source airflow-env/bin/activate
+```
+
+Using conda (You may need to install conda first)
+```
+conda create --name airflow-env python=3.8
+conda activate airflow-env
+```
+
 ### Installation Steps
 
+1. Set Airflow Home:
+Airflow requires a home directory, and uses ~/airflow by default, but you can set a different location if you prefer. The AIRFLOW_HOME environment variable is used to inform Airflow of the desired location. This step of setting the environment variable should be done before installing Airflow so that the installation process knows where to store the necessary files.
+
+```
+export AIRFLOW_HOME=~/airflow
+```
+
+Do a quick check to see if the variable is set correctly:
+
+```
+echo $AIRFLOW_HOME
+```
+
+2. Install Airflow:
+```
+AIRFLOW_VERSION=2.7.3
+
+# Extract the version of Python you have installed. If you're currently using a Python version that is not supported by Airflow, you may want to set this manually.
+# See above for supported versions.
+PYTHON_VERSION="$(python --version | cut -d " " -f 2 | cut -d "." -f 1-2)"
+
+CONSTRAINT_URL="https://raw.githubusercontent.com/apache/airflow/constraints-${AIRFLOW_VERSION}/constraints-${PYTHON_VERSION}.txt"
+# For example this would install 2.7.3 with python 3.8: https://raw.githubusercontent.com/apache/airflow/constraints-2.7.3/constraints-3.8.txt
+
+pip install "apache-airflow==${AIRFLOW_VERSION}" --constraint "${CONSTRAINT_URL}"
+```
+
+3. Run Airflow Standalone 
+
+This initializes the database, creates a user, and starts all components:
+
+```
+airflow standalone
+```
+
+4. Create a DAGs folder (assuming you're in the home directory and you've set AIRFLOW_HOME to ~/airflow)
+```
+mkdir ~/airflow/dags
+```
